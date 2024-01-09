bb36d456e2fec2934fead2832d8a1cec1873336c
Yeferson <yguerraq@unjbg.edu.pe>
2024-01-09 02:01:52 -0500

    borre 1

---
+++ sorter.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+
+template <typename T>
+void swap(T &a, T &b) {
+    T temp = a;
+    a = b;
+    b = temp;
+}
+
+template <typename T>
+void bubbleSort(T arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                // Si el elemento actual es mayor que el siguiente, intercambiamos
+                swap(arr[j], arr[j + 1]);
+            }
+        }
+    }
+}
+
+template <typename T>
+void imprimirArreglo(T arr[], int size) {
+    for (int i = 0; i < size; i++) {
+        std::cout << arr[i] << " ";
+    }
+    std::cout << std::endl;
+}
+
+int main() {
+    // Ejemplo con números enteros
+    int arregloEnteros[] = {64, 25, 12, 22, 11};
+    int nEnteros = sizeof(arregloEnteros) / sizeof(arregloEnteros[0]);
+
+    std::cout << "Arreglo original (enteros): ";
+    imprimirArreglo(arregloEnteros, nEnteros);
+    bubbleSort(arregloEnteros, nEnteros);
+    std::cout << "Arreglo ordenado (enteros): ";
+    imprimirArreglo(arregloEnteros, nEnteros);
+
+    // Ejemplo con caracteres
+    char arregloCaracteres[] = {'b', 'a', 'd', 'c', 'e'};
+    int nCaracteres = sizeof(arregloCaracteres) / sizeof(arregloCaracteres[0]);
+
+    std::cout << "Arreglo original (caracteres): ";
+    imprimirArreglo(arregloCaracteres, nCaracteres);
+    bubbleSort(arregloCaracteres, nCaracteres);
+    std::cout << "Arreglo ordenado (caracteres): ";
+    imprimirArreglo(arregloCaracteres, nCaracteres);
+
+    return 0;
+}
